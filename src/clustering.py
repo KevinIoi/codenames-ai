5f06af46a4ec497b11748d212c6a9340aad0c3c4
kevin <kevinioi@outlook.com>
2020-05-10 14:56:05 -0400

    surpressed numpy warning

--- src/clustering.py
+++ src/clustering.py
@@ -1,6 +1,9 @@
 import numpy as np
 import copy
 
+import warnings
+warnings.filterwarnings('ignore')
+
 # distance functions
 def cosine_dist(x, y):
     return np.dot(x, y) / (np.sqrt(np.dot(x, x)) * np.sqrt(np.dot(y, y)))
